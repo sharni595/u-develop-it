6810ebf26e53f6a2171019464f7c532119ea029f
Sharni Zaugg <sharni.greener@gmail.com>
2021-05-24 12:28:37 -0600

    set up sql connection

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'kool123kat',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //default for any other request(not found)
 app.use((req, res) => {
