125c2b530a7792e8f44f6552397b6b92a3854042
Sharni Zaugg <sharni.greener@gmail.com>
2021-05-26 18:26:25 -0600

    clean code and create routes

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'kool123kat',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+module.exports = db;
